dda2702ad49d70ffb9034d2e8946bc45695b2961
cha yong jun <perfect_yon@naver.com>
2024-12-01 19:50:39 +0900

    add: socket.io dependency

---
+++ Socket/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Socket
+//
+//  Created by Yong Jun Cha on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Socket/SocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketApp.swift
+//  Socket
+//
+//  Created by Yong Jun Cha on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
